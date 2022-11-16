c8d8ffcb3d62445aa106874c3822f27e1140b084
pless <prathamaclinton@gmail.com>
2022-11-16 17:41:51 +0800

    Completed 16x16 square of divs

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Etch-a-Sketch
+
+A project to flex my DOM manipulation skills. Aim is to build a browser version of something between a sketchpad and and Etch-A-Skecth.
+
+First Commit 16/11/2022
+- Completed 16x16 grid of square divs 
---
+++ index.js
@@ -0,0 +1,14 @@
+let container = document.getElementById('container');
+
+function createDiv(){
+for (let i = 1; i<=16; i++) {
+    let div = document.createElement('div');
+    div.className = 'gridHolder';
+    for (let j=1 ; j<=16; j++){
+        let div2 = document.createElement('div');
+        div2.className ='grid';
+        div.appendChild(div2);
+    }
+    container.appendChild(div);
+}
+}
