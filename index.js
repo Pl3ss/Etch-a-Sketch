e6d2db857774dec8efdc619af6ce9c8b2f2b3403
pless <prathamaclinton@gmail.com>
2022-11-18 16:35:13 +0800

    Added button to ask user input for number of grids

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+let button = document.getElementById('button');
+
+let x = 0;
+
+function gridNumber() {
+    let y = prompt('Insert number of grids: ', '');
+    x = Number(y);
+
+    if (x <= 100) {
+        createDiv();
+    } else {
+        alert('Error! Maximum input number is 100');
+    }
+}
+
+
 let container = document.getElementById('container');
 
 
 function createDiv(){
-for (let i = 1; i<=16; i++) {
+for (let i = 1; i<=x; i++) {
     let div = document.createElement('div');
     div.className = 'gridHolder';
 
-    for (let j=1 ; j<=16; j++){
+    for (let j=1 ; j<=x; j++){
         let div2 = document.createElement('div');
         div2.className ='grid';
         div.appendChild(div2);
@@ -14,5 +30,7 @@ for (let i = 1; i<=16; i++) {
     container.appendChild(div);
 }
 }
+
+
 
 
